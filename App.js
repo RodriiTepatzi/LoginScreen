fa259546e0a915b3845489241b41b2592772c39a
RodriiTepatzi <rodrif19@hotmail.com>
2024-03-06 23:35:59 -0600

    adding design

--- App.js
+++ App.js
@@ -1,11 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, StyleSheet, Alert} from 'react-native';
 
-export default function App() {
+const App = () => {
+
+  const [expediente, setExpediente] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    if (expediente.length !== 6) {
+      Alert.alert("El expediente debe tener 6 caracteres");
+    } else if (password.length < 8) {
+      Alert.alert("El password debe ser mayor o igual a 8 caracteres");
+    } else {
+      Alert.alert("Login exitoso");
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Login</Text>
+      </View>
+      <View style={styles.inputsContainer}>
+        <TextInput 
+          style={styles.inputExpediente} 
+          placeholder="Expediente" 
+          value={expediente} 
+          keyboardType='numeric'
+          maxLength={6}
+          onChangeText = {(text)=> {
+            setExpediente(text)
+          }}
+          />
+        <TextInput 
+          style={styles.inputPassword} 
+          placeholder="Password" 
+          multiline={false}
+          secureTextEntry={true} 
+          onChangeText = {(text)=> {
+            setPassword(text)
+          }}
+          />
+      </View>
+      <View style={styles.buttonContainer}>
+      <Button
+        color="white"
+        style={styles.button}
+        title="Login"
+        onPress={handleSubmit}/>
+      </View>
     </View>
   );
 }
@@ -13,8 +56,47 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff',
   },
+  titleContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 30,
+  },
+  inputsContainer: {
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  inputExpediente: {
+    borderBottomWidth: 0,
+    marginBottom: 20,
+    borderRadius: 5,
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+  },
+  inputPassword: {
+    borderBottomWidth: 0,
+    borderRadius: 5,
+    backgroundColor: '#f2f2f2',
+    paddingHorizontal: 10,
+    paddingVertical: 15,
+  },
+  buttonContainer: {
+    marginHorizontal: 20,
+    paddingVertical: 3,
+    backgroundColor: '#040404',
+    borderRadius: 5,
+  },
+  button: {
+    width: '100%',
+    padding: 10,
+    borderRadius: 5,
+  },
+  
+
 });
+export default App;
